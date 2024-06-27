244e77336e8825652a04507685a7ccb3dac47dd4
Vinnu_dev <vinodkumarhulebeedu@gmail.com>
2024-06-27 20:19:52 +0530

    Updation

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest
 ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your
-dining experience, one delicious meal at a time.</p>
+dining experience, one delicious meal at a time.....</p>
         <button><a href='#explore-menu'>View Menu</a></button> 
       </div>
     </div>
